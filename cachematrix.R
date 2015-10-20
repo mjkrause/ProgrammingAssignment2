8f78e7f58b704a8eecfaf4aaa119adc79c080e02
snapclust <michael.j.krause@gmail.com>
2015-10-19 18:18:13 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -25,15 +25,61 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-        m <- x$getinverse()
+        m <- x$getinverse() # try to get inverse matrix from cache
+        # if m is cached already just return it ...
         if(!is.null(m)) {
                 message("getting cached data")
-                return(m)
+                return(m) # return inverse matrix from cache
         }
-        data <- x$get()
-        m <- solve(data)
-        x$setinverse(m)
-        m
+	# ... if m was empty obtain original matrix and write into
+	# variable "data"
+        data <- x$get()	
+        m <- solve(data) # then get inverse of matrix data and write to m
+        x$setinverse(m) 
+        m # return inverse matrix from data
 
 
 }
+
+## RUNNING A SESSION WITH THE CODE
+
+## read file
+#> source('cachematrix.R')
+## Create a simple 3x3 matrix.
+#> A=makeCacheMatrix(matrix(c(1,0,1,0,2,1,1,1,1),nrow=3,ncol=3))
+## Check whether the matrix was created correctly:
+#> A$get()
+#     [,1] [,2] [,3]
+#[1,]    1    0    1
+#[2,]    0    2    1
+#[3,]    1    1    1
+
+# Try getting the inverse of A:
+#> A$getinverse()
+#NULL
+
+## It failed because the code looked for it in the cache. The inverse
+## of A has not been created yet because "cacheSolve" has not been run 
+## yet.
+
+## run cacheSolve(A)to obtain the inverse of A:
+#> cacheSolve(A)
+#     [,1] [,2] [,3]
+#[1,]   -1   -1    2
+#[2,]   -1    0    1
+#[3,]    2    1   -2
+
+# Repeating the command now yields the inverse of A:
+#> A$getinverse()
+#     [,1] [,2] [,3]
+#[1,]   -1   -1    2
+#[2,]   -1    0    1
+#[3,]    2    1   -2
+
+## Repeating cacheSolve now retrieves the inverse of A from cache:
+#> cacheSolve(A)
+#getting cached data
+#     [,1] [,2] [,3]
+#[1,]   -1   -1    2
+#[2,]   -1    0    1
+#[3,]    2    1   -2
